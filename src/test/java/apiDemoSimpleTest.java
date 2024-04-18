d64446a5b6f9025612acc054eba4c6b2cc39a75f
Taras Shevchenko <t.shevchenko.1981@gmail.com>
2024-04-18 20:21:13 +0300

    add windowsTest class and apiTest (#4)

---
+++ src/test/java/apiDemoSimpleTest.java
@@ -0,0 +1,20 @@
+import com.microsoft.playwright.APIResponse;
+import core.BasePlayWrightTest;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class apiDemoSimpleTest extends BasePlayWrightTest {
+
+
+    @Test
+    public void apiResponeTest(){
+        APIResponse response1 = page.request().get("https://demoqa.com/browser-windows");
+        System.out.println(response1.headers());
+        System.out.println("<<" + response1.status() + " " + response1.url());
+        assertThat(response1).isOK();
+    }
+
+}
