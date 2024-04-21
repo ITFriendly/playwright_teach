e9596c87f1b652e58a97b65ed8118c363c0975c3
tryam <tryamkin@gmail.com>
2024-04-21 20:41:43 +0300

    add framestests

--- src/test/java/alertsFramesWindows/Frames.java
+++ src/test/java/alertsFramesWindows/Frames.java
@@ -1,4 +1,33 @@
 package alertsFramesWindows;
 
-public class Frames {
+import com.microsoft.playwright.Frame;
+import com.microsoft.playwright.FrameLocator;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.options.AriaRole;
+import core.BasePlayWrightTest;
+import org.testng.annotations.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class Frames extends BasePlayWrightTest {
+
+    @Test
+    void fram1test() {
+        page.navigate("https://demoqa.com/frames");
+        assertThat(page.locator("#framesWrapper")).containsText("Frames");
+        page.frameLocator("#frame1").getByRole(AriaRole.HEADING, new FrameLocator.GetByRoleOptions().setName("This is a sample page")).click();
+        assertThat(page.frameLocator("#frame1").locator("#sampleHeading")).containsText("This is a sample page");
+        Locator framelocator = page.frameLocator("#frame1").locator("body");
+        System.out.println(framelocator.textContent());
+    }
+    @Test
+    void fram2test() {
+        page.navigate("https://demoqa.com/frames");
+        FrameLocator frameLocator2 =  page.frameLocator("#frame2");
+        System.out.println(frameLocator2.getByTitle("#frame2"));
+        assertThat(page.frameLocator("#frame2").locator("body")).containsText("This is a sample page");
+       // assertThat(frameLocator2.getByTitle("#frame2")).containsText("frame2");
+        System.out.println( page.frameLocator("#frame2").locator("body"));
+
+    }
 }
